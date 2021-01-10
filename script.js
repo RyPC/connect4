828916cf74091b4e5ec547f6a9ce07ab6a43c290
RyPC <30598611+RyPC@users.noreply.github.com>
2021-01-10 14:30:43 -0700

    win conditions
    added win conditions for horizontal, vertical, and diagonal patterns

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const boardWidth = 1000;
 const squareLen = Math.floor(boardWidth / 7);
 var level = [5, 5, 5, 5, 5, 5, 5];
-var turn = true;
+//true - blue, false - red
+var turn = Math.random() > 0.5;
 const pieceSize = 100;
-
-var board = [];
-var row = [0, 0, 0, 0, 0, 0, 0];
-for (var i = 0; i < 6; i++) {
-    board.push(row);
-}
-
+var board = [[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0]];
+var won = false;
 
 function start() {
 
@@ -35,12 +36,51 @@ function createBoxes() {
 }
 
 function move(row, col) {
+    if (level[col] < 0 || won) return;
     document.getElementById(`row${level[col]}col${col}`).innerHTML = `<div class="piece ${turn ? "blue" : "red"}" style="height: ${pieceSize}px; width: ${pieceSize}px;"></div>`;
+    board[level[col]][col] = turn ? 1 : -1;
     turn = !turn;
     level[col]--;
     checkWin();
 }
 
 function checkWin() {
+    for (var i = 5; i >= 0; i--) {
+        var row = board[i];
+        for (var j = 0; j < 4; j++) {
+            if (row[j] != 0 && row[j] === row[j + 1] && row[j] === row[j + 2] && row[j] === row[j + 3]) {
+                win(row[j] === 1 ? "Blue" : "Red");
+                return;
+            }
+            if (i > 2) {
+                if (row[j] != 0 && row[j] === board[i - 1][j + 1] && row[j] === board[i - 2][j + 2] && row[j] === board[i - 3][j + 3]) {
+                    win(row[j] === 1 ? "Blue" : "Red");
+                    return;
+                }
+            }
+            if(i < 3) {
+                if (row[j] != 0 && row[j] === board[i + 1][j + 1] && row[j] === board[i + 2][j + 2] && row[j] === board[i + 3][j + 3]) {
+                    win(row[j] === 1 ? "Blue" : "Red");
+                    return;
+                }
+            }
+            if (row[j] != 0 && row[j] === row[j + 1] && row[j] === row[j + 2] && row[j] === row[j + 3]) {
+                win(row[j] === 1 ? "Blue" : "Red");
+                return;
+            }
+        }
+    }
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 7; j++) {
+            if (board[i][j] != 0 && board[i][j] === board[i + 1][j] && board[i][j] === board[i + 2][j] && board[i][j] === board[i + 3][j]) {
+                win(row[j] === 1 ? "Blue" : "Red");
+                return;
+            }
+        }
+    }
+}
 
+function win(winner) {
+    alert(`${winner} Wins!!`);
+    won = true;
 }
