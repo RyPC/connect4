b6da111067b3a7210bdb9f2afca7b68268df64aa
Ryan Su <ryan.parkcity@gmail.com>
2024-01-23 21:12:16 -0800

    Updated to use breadth-first search for win checking

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function createBoxes() {
 
     for (var i = 0; i < 6; i++) {
         for (var j = 0; j < 7; j++) {
-            html+= `<div onclick="move(${i}, ${j});" style="padding:${(1 - (pieceSize / squareLen)) * 50}%; width:${squareLen}; height:${squareLen}" id="row${i}col${j}" class="square"></div>`;
+            html+= `<div onclick="move(${j});" style="padding:${(1 - (pieceSize / squareLen)) * 50}%; width:${squareLen}; height:${squareLen}" id="row${i}col${j}" class="square"></div>`;
         }
     }
 
@@ -35,52 +35,55 @@ function createBoxes() {
     
 }
 
-function move(row, col) {
+function move(col) {
     "use strict";
+    // don't let move if invalid move or game is over
     if (level[col] < 0 || won) {return;}
+
     document.getElementById(`row${level[col]}col${col}`).innerHTML = `<div class="piece ${turn ? "blue" : "red"}" style="height: ${pieceSize}px; width: ${pieceSize}px;"></div>`;
     board[level[col]][col] = turn ? 1 : -1;
+
+    checkWin(level[col], col);
+
     turn = !turn;
     level[col]--;
-    checkWin();
 }
 
-function checkWin() {
+function inBounds(row, col) {
+    "use strict"
+    return !(row > 5 || row < 0 || col > 6 || col < 0);
+}
+
+function checkWin(row, col) {
+    const directions = [[1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]]
+    for (var i = 0; i < directions.length; i++) {
+        // check for in-bounds in each direction before checking
+        if (inBounds(row + (directions[i][1] * 3), col + (directions[i][0] * 3)) && 
+            checkWinCondition(row, col, directions[i][0], directions[i][1], 1)) {
+            win(board[row][col] === 1 ? "Blue" : "Red");
+            return;
+        }
+    }
+}
+
+// row = row of placed piece
+// col = column of placed piece
+// x = increased col to check
+// y = increased row to check
+// i = how far from placed piece
+function checkWinCondition(row, col, x, y, i) {
     "use strict";
-    var i, j, row;
-    for (i = 5; i >= 0; i--) {
-        row = board[i];
-        for (j = 0; j < 4; j++) {
-            if (row[j] !== 0 && row[j] === row[j + 1] && row[j] === row[j + 2] && row[j] === row[j + 3]) {
-                win(row[j] === 1 ? "Blue" : "Red");
-                return;
-            }
-            if (i > 2) {
-                if (row[j] !== 0 && row[j] === board[i - 1][j + 1] && row[j] === board[i - 2][j + 2] && row[j] === board[i - 3][j + 3]) {
-                    win(row[j] === 1 ? "Blue" : "Red");
-                    return;
-                }
-            }
-            if(i < 3) {
-                if (row[j] !== 0 && row[j] === board[i + 1][j + 1] && row[j] === board[i + 2][j + 2] && row[j] === board[i + 3][j + 3]) {
-                    win(row[j] === 1 ? "Blue" : "Red");
-                    return;
-                }
-            }
-            if (row[j] !== 0 && row[j] === row[j + 1] && row[j] === row[j + 2] && row[j] === row[j + 3]) {
-                win(row[j] === 1 ? "Blue" : "Red");
-                return;
-            }
-        }
+    if (i >= 4) {
+        return true;
     }
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 7; j++) {
-            if (board[i][j] !== 0 && board[i][j] === board[i + 1][j] && board[i][j] === board[i + 2][j] && board[i][j] === board[i + 3][j]) {
-                win(row[j] === 1 ? "Blue" : "Red");
-                return;
-            }
-        }
+
+    var piece = board[row + (y * i)][col + (x * i)];
+    var originalPiece = board[row][col];
+    if (piece != originalPiece) {
+        return false;
     }
+
+    return checkWinCondition(row, col, x, y, (i + 1));
 }
 
 function win(winner) {
